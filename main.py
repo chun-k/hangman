1c111e131d97957240cd93bf6c812310dae72286
chun-k <152246205+chun-k@users.noreply.github.com>
2023-12-19 14:30:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import random
 from hangpkg.hang_class import *
 
+HANGMAN = [
+'________',
+'|       |',
+'|       O',
+'|       |',
+'|      /|\ ',
+'|       |',
+'|      / \ '
+]
+
+WORDS = [
+    'casa', 'car', 'mono', 'elevator', 'python', 'java',
+    'pleasure', 'young', 'festival', 'sing', 'class'
+]
+
+
 if __name__ == '__main__':
     word_to_guess = random.choice(WORDS)
     hangman = Hangman(word_to_guess)
